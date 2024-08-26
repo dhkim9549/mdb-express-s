bdbfdab0d07f3d211d431975a055ab6212c8088d
dhkim9549 <dhkim95@hotmail.com>
2024-08-26 01:19:55 +0000

    add getAptTrd

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import log4js from 'log4js';
 
 import getData from './api/get-data.js';
 import postData from './api/post-data.js';
+import getAptTrd from './api/get-apt-trd.js';
 
 dotenv.config();
 log4js.configure("./config/log4js.json");
@@ -19,6 +20,7 @@ app.use(express.json()) // for parsing application/json
 
 app.get('/api/get-data', getData)
 app.post('/api/post-data', postData)
+app.get('/api/get-apt-trd', getAptTrd);
 
 app.listen(port, () => {
   logger.info(`Example app listening on port ${port}`)
