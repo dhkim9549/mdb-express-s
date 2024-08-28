6bf5ab53227410d60dba226281944e19108d8efd
dhkim9549 <dhkim95@hotmail.com>
2024-08-28 13:54:13 +0000

    add get-apt-nm

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import log4js from 'log4js';
 import getData from './api/get-data.js';
 import postData from './api/post-data.js';
 import getAptTrd from './api/get-apt-trd.js';
+import getAptNm from './api/get-apt-nm.js';
 
 dotenv.config();
 log4js.configure("./config/log4js.json");
@@ -21,6 +22,7 @@ app.use(express.json()) // for parsing application/json
 app.get('/api/get-data', getData)
 app.post('/api/post-data', postData)
 app.get('/api/get-apt-trd', getAptTrd);
+app.get('/api/get-apt-nm', getAptNm);
 
 app.listen(port, () => {
   logger.info(`Example app listening on port ${port}`)
