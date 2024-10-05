75ed12edbd286d097368440f0e8143a55cd14284
dhkim9549 <dhkim95@hotmail.com>
2024-10-05 09:45:05 +0000

    first commit

---
+++ conn.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+import log4js from 'log4js';
+
+dotenv.config();
+const logger = log4js.getLogger("app");
+const client = new MongoClient(process.env.MONGODB_URI);
+await client.connect();
+const db = client.db("dbApt");
+
+export { db };
