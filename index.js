8af653a1b6da5249e4861b8fbd3954afad7d0ec6
dhkim9549 <dhkim95@hotmail.com>
2024-10-05 09:47:45 +0000

    remove unnecessary endpoint

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import log4js from 'log4js';
 
-import getData from './api/get-data.js';
-import postData from './api/post-data.js';
 import getAptTrd from './api/get-apt-trd.js';
 import getAptInfo from './api/get-apt-info.js';
 
@@ -11,16 +9,12 @@ dotenv.config();
 log4js.configure("./config/log4js.json");
 
 const logger = log4js.getLogger("app");
-logger.info("this should give me a line number now");
-
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(log4js.connectLogger(log4js.getLogger("http"), { level: 'auto' }));
-app.use(express.json()) // for parsing application/json
+app.use(express.json());
 
-app.get('/api/get-data', getData)
-app.post('/api/post-data', postData)
 app.get('/api/get-apt-trd', getAptTrd);
 app.get('/api/get-apt-info', getAptInfo);
 
