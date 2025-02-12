1d8006cc3abc1654ee3394be5bc64a840919ab18
dhkim9549 <dhkim95@hotmail.com>
2025-02-12 07:41:17 +0000

    prettier

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import log4js from 'log4js';
+import express from "express";
+import dotenv from "dotenv";
+import log4js from "log4js";
 
-import getAptTrd from './api/get-apt-trd.js';
-import getAptInfo from './api/get-apt-info.js';
+import getAptTrd from "./api/get-apt-trd.js";
+import getAptInfo from "./api/get-apt-info.js";
 
 dotenv.config();
-log4js.configure('./config/log4js.json');
+log4js.configure("./config/log4js.json");
 
 const logger = log4js.getLogger("app");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(log4js.connectLogger(log4js.getLogger("http"), { level: 'auto' }));
+app.use(log4js.connectLogger(log4js.getLogger("http"), { level: "auto" }));
 app.use(express.json());
 
-app.get('/api/get-apt-trd', getAptTrd);
-app.get('/api/get-apt-info', getAptInfo);
+app.get("/api/get-apt-trd", getAptTrd);
+app.get("/api/get-apt-info", getAptInfo);
 
 app.listen(port, () => {
-  logger.info(`Example app listening on port ${port}`)
-})
+  logger.info(`Example app listening on port ${port}`);
+});
