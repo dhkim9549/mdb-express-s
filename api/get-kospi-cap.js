969071aae2e55ca163b1876a440837971efc3ff1
dhkim9549 <dhkim95@hotmail.com>
2025-02-23 08:46:09 +0000

    first commit

---
+++ api/get-kospi-cap.js
@@ -0,0 +1,32 @@
+import log4js from "log4js";
+import { dbFF } from "../conn.js";
+
+const logger = log4js.getLogger("app");
+
+export default async function getKospiCap(req, res) {
+  logger.info("getKospiCap() start...");
+
+  logger.info("req.query = " + JSON.stringify(req.query));
+
+  let query = {};
+
+  const options = {
+    sort: { N: 1 },
+    projection: { _id: 0, N: 1, name: 1, cap: 1 },
+  };
+
+  logger.info({ query, options });
+
+  let resData = [];
+
+  try {
+    const collection = dbFF.collection("colKospi");
+    resData = await collection.find(query, options).limit(5000).toArray();
+  } catch (error) {
+    throw error;
+  }
+
+  res.json(resData);
+
+  logger.info("getKospiCap() end...");
+}
