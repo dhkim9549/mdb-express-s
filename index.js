010761923d0582d567471dd47864d6748c3844a5
root <dhkim95@hotmail.com>
2025-08-22 09:07:53 +0000

    add APIs

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import log4js from "log4js";
 import getAptTrd from "./api/get-apt-trd.js";
 import getAptInfo from "./api/get-apt-info.js";
 import getKospiCap from "./api/get-kospi-cap.js";
+import getLoanRate from "./api/get-loan-rate.js";
+import getStockInfo from "./api/get-stock-info.js";
 
 dotenv.config();
 log4js.configure("./config/log4js.json");
@@ -19,6 +21,8 @@ app.use(express.json());
 app.get("/api/get-apt-trd", getAptTrd);
 app.get("/api/get-apt-info", getAptInfo);
 app.get("/api/get-kospi-cap", getKospiCap);
+app.get("/api/get-loan-rate", getLoanRate);
+app.get("/api/get-stock-info", getStockInfo);
 
 app.listen(port, () => {
   logger.info(`Example app listening on port ${port}`);
