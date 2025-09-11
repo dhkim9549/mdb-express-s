66049298ba4eb65e468a1d637d79b5e154fcefd1
root <dhkim95@hotmail.com>
2025-09-11 08:27:28 +0000

    add getAptLog

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import log4js from "log4js";
 
 import getAptTrd from "./api/get-apt-trd.js";
 import getAptInfo from "./api/get-apt-info.js";
+import getAptLog from "./api/get-apt-log.js";
 import getKospiCap from "./api/get-kospi-cap.js";
 import getLoanRate from "./api/get-loan-rate.js";
 import getStockInfo from "./api/get-stock-info.js";
@@ -20,7 +21,10 @@ app.use(express.json());
 
 app.get("/api/get-apt-trd", getAptTrd);
 app.get("/api/get-apt-info", getAptInfo);
+app.get("/api/get-apt-log", getAptLog);
+
 app.get("/api/get-kospi-cap", getKospiCap);
+
 app.get("/api/get-loan-rate", getLoanRate);
 app.get("/api/get-stock-info", getStockInfo);
 
